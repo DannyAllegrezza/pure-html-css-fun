401193fed7d2924966c3a0d075d5071e7cb617ee
Danny Allegrezza <danny@dannyallegrezza.com>
2020-11-03 14:39:38 -0500

    log y-scroll position

--- src/progress-bar/progress-bar.js
+++ src/progress-bar/progress-bar.js
@@ -1,1 +1,21 @@
-console.log("progress bar!");
+window.addEventListener("scroll", function (event) {
+  // console.log(document.body.scrollHeight);
+  // console.log(window.innerHeight);
+  // let yPosition = this.scrollY;
+  // console.log(`The current Y position is ${yPosition}`);
+  // var distanceFromBottom =
+  //   document.body.scrollHeight - window.innerHeight - window.scrollY;
+  // console.log(`Distance from bottom: ${distanceFromBottom}`);
+});
+
+document.onscroll = function () {
+  let parent = document.body.parentNode;
+
+  console.log("scroll top", parent.scrollTop);
+  console.log("scrollHeight", parent.scrollHeight);
+  console.log("clientHeight", parent.clientheight);
+  let position =
+    (parent.scrollTop / (parent.scrollHeight - parent.clientHeight)) * 100;
+
+  console.log(position);
+};
