301e7fe3ae81836a8525ee5eecedad1996df183c
Danny Allegrezza <danny@dannyallegrezza.com>
2020-11-03 20:00:47 -0500

    implement simple progress bar

--- src/progress-bar/progress-bar.js
+++ src/progress-bar/progress-bar.js
@@ -1,21 +1,8 @@
-window.addEventListener("scroll", function (event) {
-  // console.log(document.body.scrollHeight);
-  // console.log(window.innerHeight);
-  // let yPosition = this.scrollY;
-  // console.log(`The current Y position is ${yPosition}`);
-  // var distanceFromBottom =
-  //   document.body.scrollHeight - window.innerHeight - window.scrollY;
-  // console.log(`Distance from bottom: ${distanceFromBottom}`);
-});
-
 document.onscroll = function () {
   let parent = document.body.parentNode;
+  let progressBar = document.getElementsByClassName("progress-bar")[0];
 
-  console.log("scroll top", parent.scrollTop);
-  console.log("scrollHeight", parent.scrollHeight);
-  console.log("clientHeight", parent.clientheight);
-  let position =
-    (parent.scrollTop / (parent.scrollHeight - parent.clientHeight)) * 100;
+  let position = Math.floor((parent.scrollTop / (parent.scrollHeight - parent.clientHeight)) * 100);
 
-  console.log(position);
+  progressBar.style.width = `${position}%`;
 };
